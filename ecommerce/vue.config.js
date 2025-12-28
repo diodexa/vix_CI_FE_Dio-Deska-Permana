41acf733fe5e1696e5d4256efcc0a531bf1b2e38
Dio <diodexa@gmail.com>
2025-12-29 02:00:51 +0700

    fixing ghpages4
    fixing ghpages4

--- ecommerce/vue.config.js
+++ ecommerce/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: '/vix_CI_FE_Dio-Deska-Permana/',
-  transpileDependencies: true
+  outputDir: '../dist',
+  publicPath: './'
 })
